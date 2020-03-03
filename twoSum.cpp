b1cab845d43a6b56f6e4edcfa28a5ad49c90d880
Ankit Batra <ankitb@xsense.ai>
2020-03-03 15:40:52 -0800

    Here comes the FIRST solution: "two sum" :)
    Signed-off-by: Ankit Batra <ankitb@xsense.ai>

---
+++ twoSum.cpp
@@ -0,0 +1,58 @@
+//
+// Created by ankitb on 2/28/20.
+//
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        int len = nums.size();
+        std::vector<int> indices(2);
+        std::unordered_map<int, int> numToIdx;
+
+        for(int i = 0; i < len; ++i) {
+            if(numToIdx.find(target - nums[i]) != numToIdx.end()) {
+                indices[0] = numToIdx[target - nums[i]];
+                indices[1] = i;
+            }
+            else
+                numToIdx[nums[i]] = i;
+        }
+        return indices;
+    }
+
+    void printVector(std::vector<int>& nums) {
+        int n = nums.size();
+
+        for(int i = 0; i < n; ++i)
+            std::cout << nums[i] << " ";
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    int number;
+    int target;
+    std::string line;
+    std::vector<int> indices;
+    std::vector<int> numbers;
+    Solution obj;
+
+    std::cout << "Enter the numbers separated by spaces: " << std::endl;
+    std::getline(std::cin, line);
+    std::istringstream stream(line);
+    while(stream >> number)
+        numbers.push_back(number);
+
+    std::cout << "Enter the target value: " << std::endl;
+    std::cin >> target;
+
+    indices = obj.twoSum(numbers, target);
+    std::cout << "The indices whose value adds up to target: " << std::endl;
+    obj.printVector(indices);
+
+    return 0;
+}
